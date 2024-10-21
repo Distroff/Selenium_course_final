ad84b15e97501ee33717c2893abf904ba2aa296e
Distroff <Teryokhin@yandex.ru>
2024-10-21 17:13:02 +0300

    Step 4.2

--- pages/locators.py
+++ pages/locators.py
@@ -1,7 +1,17 @@
 from selenium.webdriver.common.by import By
 
+
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
 
+
 class LoginPageLocators:
     LOGIN_PAGE = (By.CSS_SELECTOR, '')
+
+
+class ProductPageLocators:
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "div.alertinner ")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
