51f185dae3feab9de8bc76c158502aee9dc0a848
Distroff <Teryokhin@yandex.ru>
2024-10-30 10:09:56 +0300

    Step 4.3 step 14 final fix errors02

--- test_product_page.py
+++ test_product_page.py
@@ -6,9 +6,8 @@ from .pages.login_page import LoginPage
 
 
 @pytest.mark.need_review
-@pytest.mark.parametrize('promo_offer', ["0", "1", "2", "3", "4", "5", "6", pytest.param('7', marks=pytest.mark.xfail), "8", "9"])
-def test_guest_can_add_product_to_basket(browser, promo_offer):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+def test_guest_can_add_product_to_basket(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
     page = ProductPage(browser, link)
     page.open()
     page.press_button_add_to_basket()
